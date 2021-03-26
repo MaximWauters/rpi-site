645fab8458a29b50a3cb63bd20754e46fc908c4b
Maxim Wauters <wauters.maxim@hotmail.com>
2021-03-26 14:49:10 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+const gpio = require('../rpi-gpio').promise;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'testSlider';
+
+ points1 = 7;
+ points2 = 7;
+ t = 0;
+
+  update(x: number){
+    console.log(x);
+    this.points1 = x;
+    gpiop.setup(13, gpiop.DIR_OUT)
+    .then(() => {
+        return gpiop.write(13, x)
+    })
+    .catch((err) => {
+        console.log('Error: ', err.toString())
+    })
+  }
+
+  update2(x: number){
+    this.points2 = x;
+  }
+
+  add(){
+    this.t++;
+    console.log(this.t);
+  }
+  
+
+
 }
